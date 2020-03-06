b066b55a9d766d32de5d69c1ce31b216a4dcf54a
vishwas1 <vishwasanand.cse@gmail.com>
2020-03-06 21:21:07 +0530

    Test case for todolist -WIP

---
+++ test/todolistTest.js
@@ -0,0 +1,13 @@
+const Deployer =  require('aeproject-lib').Deployer;
+const EXAMPLE_CONTRACT_PATH = "./contracts/ToDoList.aes";
+
+
+
+describe('Todo task list contract', () => {
+    
+
+    let deployer;
+    let instance;
+    let ownerPrivKey = wallets[0];
+
+})
